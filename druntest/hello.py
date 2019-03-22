d9b10becbf49bbe1a24682a3ede1b25d79f092fb
qtateam <44162446+qtateam@users.noreply.github.com>
2019-03-22 15:11:41 +0800

    add hello.py

---
+++ druntest/hello.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+'''示例测试用例
+'''
+#2019/03/22 QTAF自动生成
+
+from drunlib.testcase import DrunTestCase
+
+class HelloTest(DrunTestCase):
+    '''示例测试用例
+    '''
+    owner = "root"
+    timeout = 5
+    priority = DrunTestCase.EnumPriority.High
+    status = DrunTestCase.EnumStatus.Design
+    
+    def run_test(self):
+        self.log_info("hello testcase")
+        self.assert_equal(True, True)
+    
+if __name__ == '__main__':
+    HelloTest().debug_run()
+
